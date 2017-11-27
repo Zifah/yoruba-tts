c7a267562f15632aed46b427237923c790a66d99
Hafiz Adewuyi <hafiz@chatdesk.com>
2017-11-27 14:17:57 +0100

    Hashtag correction

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -212,7 +212,7 @@ $(function () {
         $("#share-facebook").attr("link", longUrl);
         $("#share-facebook").attr("text", twitterText);
         $("#share-twitter").attr("href", "https://twitter.com/intent/tweet?url=" + shortUrl + "&text="
-          + twitterText + "&hashtags=TTSYoruba&via=TTSYorùbá");
+          + twitterText + "&hashtags=TTSYoruba&via=TTSYoruba");
         $("#share-link").attr("link", shortUrl);
         $("meta[property='og:description']").attr("content", twitterText);
       }).fail(function () {
